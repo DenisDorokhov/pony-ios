cf912884ad6934454da50d0eb6a7a52b2f4898d8
ddorokhov <denis.dorokhov@insparx.com>
2017-01-08 23:35:46 +0100

    tests: increase blocking operation timeout to 60

--- PonyTests/Sources/Common/ObservableConvertibleType+PonyTests.swift
+++ PonyTests/Sources/Common/ObservableConvertibleType+PonyTests.swift
@@ -8,6 +8,6 @@ import RxBlocking
 
 extension ObservableConvertibleType {
     public func toTestBlocking() -> BlockingObservable<E> {
-        return toBlocking(timeout: 30)
+        return toBlocking(timeout: 60)
     }
 }
